bae0351b9ced82aacddbb4c3ce64c1bbfc2d5f73
Mr Speaker <mrspeaker@gmail.com>
2014-04-24 08:40:51 -0400

    add pixelmator -> png converter

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
 var gulp = require("gulp");
+var spawn = require('child_process').spawn;
 
 gulp.task("default", function () {
 
+});
+
+gulp.watch("res/images/**/*.pxm", function (e) {
+
+	if (e.path.indexOf(".pxm") === -1) {
+		console.log("wtf gulp?");
+		return;
+	}
+
+    spawn("./pxm2png", [e.path], {cwd: process.cwd() + "/vendor"});
+
+    console.log("updated:", e.path)
 });
