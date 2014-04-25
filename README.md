576d18067eea18b7e897e8e5c7c8f69b618bfb01
Mr Speaker <mrspeaker@gmail.com>
2014-04-25 15:02:19 -0400

    transformers for resources

--- README.md
+++ README.md
@@ -3,7 +3,12 @@
 # Setup build
 
 npm install --save-dev gulp
+npm install --save-dev gulp-es6-transpiler
 
 # Build
 
 gulp
+
+
+ES6 options:
+https://github.com/termi/es6-transpiler
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,58 @@
-var gulp = require("gulp");
-var spawn = require('child_process').spawn;
+var gulp = require("gulp"),
+	es6transpiler = require('gulp-es6-transpiler'),
+	spawn = require('child_process').spawn,
+
+	imgGlob = "res/images/**/*.pxm",
+	audioGlob = "res/audio/**/*.wav",
+	srcGlob = "src/**/*.js";
 
 gulp.task("default", function () {
-
+	return gulp.src(srcGlob)
+        .pipe(es6transpiler())
+        .pipe(gulp.dest("scripts"));
 });
 
-gulp.watch("res/images/**/*.pxm", function (e) {
+gulp.task("watch", function() {
+    gulp.watch(srcGlob, ["default"]);
+    gulp.watch(imgGlob, pxm2png);
+    gulp.watch(audioGlob, wav2comp);
+});
 
+function pxm2png (e) {
+	//console.log("PXM:", e);
 	if (e.path.indexOf(".pxm") === -1) {
 		console.log("wtf gulp?");
+		return;
+	}
+	var path = e.path.split("/"),
+		file = path[path.length - 1];
+
+	if (e.type === "deleted") {
+		console.log("deleted:", file)
 		return;
 	}
 
     spawn("./pxm2png", [e.path], {cwd: process.cwd() + "/vendor"});
 
-    console.log("updated:", e.path)
-});
+    console.log("PNG'd:", file);
+};
+
+function wav2comp (e) {
+	//console.log("WAV:", e);
+	if (e.path.indexOf(".wav") === -1) {
+		console.log("wtf gulp?");
+		return;
+	}
+	var path = e.path.split("/"),
+		file = path[path.length - 1];
+
+	if (e.type === "deleted") {
+		console.log("deleted:", file)
+		return;
+	}
+
+	var sound = e.path.slice(0, -4);
+	spawn("ffmpeg", ["-y", "-i", sound + ".wav", sound + ".ogg", sound + ".mp3"]);
+
+    console.log("Ogg n mp3'd:", file);
+};
